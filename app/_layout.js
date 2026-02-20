096f0e52955dca48947f7e66b021c8ecb9742026
Manuwave <ejehemmanuel019@gmail.com>
2026-02-20 10:10:39 +0100

    prebuild crash apk

--- app/_layout.js
+++ app/_layout.js
@@ -2,13 +2,25 @@ import { Stack ,SplashScreen,ErrorBoundary } from "expo-router";
 import { useFonts } from "expo-font";
 import { ActivityIndicator } from "react-native";
 import {AuthProvider} from "../config/authcontext"
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
+
+
+
 
 
 
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [crashError, setCrashError] = useState(null);
+
+  useEffect(() => {
+    const originalHandler = ErrorUtils.getGlobalHandler();
+    ErrorUtils.setGlobalHandler((error, isFatal) => {
+      setCrashError(error.message + '\n' + error.stack);
+      originalHandler(error, isFatal);
+    });
+  }, []);
   const [myfonts] = useFonts({
     "Chiqueta-Regular": require("../assets/fonts/Chiqueta-Regular.ttf"),
     "Chubsy-Snack": require("../assets/fonts/Chubsy-Snack.otf"),
